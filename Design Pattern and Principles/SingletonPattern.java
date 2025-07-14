d4cf6c4ab456141007ba703ba90b7b264e2123db
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-15 01:24:15 +0530

    Update SingletonPattern.java

--- Design Pattern and Principles/SingletonPattern.java
+++ Design Pattern and Principles/SingletonPattern.java
@@ -1,1 +1,28 @@
+// Singleton Design Pattern Example in Java
+
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton instance created.");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public class SingletonPattern {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        if (s1 == s2) {
+            System.out.println("Both references point to the same Singleton object.");
+        }
+    }
+}
 
