a451cb70ae0f23e84495db032abd0e54a585e637
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-17 22:26:37 +0530

    Update update_customer_status_procedure.sql

--- Week-2/PL-SQL programming/update_customer_status_procedure.sql
+++ Week-2/PL-SQL programming/update_customer_status_procedure.sql
@@ -1,1 +1,36 @@
+-- This procedure checks the loan balance of each customer.
+-- If the balance is less than 5000, it marks the customer as 'Good'.
+-- Otherwise, it marks them as 'Review'.
+
+CREATE OR REPLACE PROCEDURE UpdateCustomerStatus IS
+
+    -- Cursor to fetch customer IDs and their loan balances
+    CURSOR customer_loans IS
+        SELECT Customers.CustomerID, Loans.Balance
+        FROM Customers
+        JOIN Loans ON Customers.CustomerID = Loans.CustomerID;
+
+BEGIN
+    -- Loop through each customer and check their loan balance
+    FOR cust IN customer_loans LOOP
+        IF cust.Balance < 5000 THEN
+            UPDATE Customers
+            SET CustomerStatus = 'Good'
+            WHERE CustomerID = cust.CustomerID;
+        ELSE
+            UPDATE Customers
+            SET CustomerStatus = 'Review'
+            WHERE CustomerID = cust.CustomerID;
+        END IF;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('Customer statuses have been updated.');
+END;
+/
+
+-- 🧪 Example usage of the procedure:
+BEGIN
+    UpdateCustomerStatus;
+END;
+/
 
