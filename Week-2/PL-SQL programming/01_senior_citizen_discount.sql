78dd0549dd84148ac903d2aebc2d683f4b04059f
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-17 21:54:14 +0530

    Update 01_senior_citizen_discount.sql

--- Week-2/PL-SQL programming/01_senior_citizen_discount.sql
+++ Week-2/PL-SQL programming/01_senior_citizen_discount.sql
@@ -1,1 +1,13 @@
+BEGIN
+    FOR cust IN (SELECT CustomerID, DOB FROM Customers) LOOP
+        IF MONTHS_BETWEEN(SYSDATE, cust.DOB)/12 > 60 THEN
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = cust.CustomerID;
+        END IF;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('Discount applied for senior citizens.');
+END;
+/
 
