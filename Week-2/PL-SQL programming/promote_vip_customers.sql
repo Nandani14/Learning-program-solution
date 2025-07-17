693acce62ab6898b753577a60adff67a8141bd4c
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-17 22:11:49 +0530

    Update promote_vip_customers.sql

--- Week-2/PL-SQL programming/promote_vip_customers.sql
+++ Week-2/PL-SQL programming/promote_vip_customers.sql
@@ -1,1 +1,13 @@
+BEGIN
+    FOR cust IN (SELECT CustomerID, Balance FROM Customers) LOOP
+        IF cust.Balance > 10000 THEN
+            UPDATE Customers
+            SET IsVIP = 'Y'
+            WHERE CustomerID = cust.CustomerID;
+        END IF;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('VIP status updated for eligible customers.');
+END;
+/
 
