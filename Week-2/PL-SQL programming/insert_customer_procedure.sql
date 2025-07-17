821a6bbb76ba089a4b99f5426cd97163e4d18a4c
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-17 22:19:52 +0530

    Update insert_customer_procedure.sql

--- Week-2/PL-SQL programming/insert_customer_procedure.sql
+++ Week-2/PL-SQL programming/insert_customer_procedure.sql
@@ -1,1 +1,13 @@
+CREATE OR REPLACE PROCEDURE InsertCustomer(
+    p_Name     IN VARCHAR2,
+    p_DOB      IN DATE,
+    p_Balance  IN NUMBER
+) AS
+BEGIN
+    INSERT INTO Customers (CustomerID, Name, DOB, Balance)
+    VALUES (Customers_seq.NEXTVAL, p_Name, p_DOB, p_Balance);
+
+    DBMS_OUTPUT.PUT_LINE('New customer added: ' || p_Name);
+END;
+/
 
