5aed55c735827ca32781b2bc9655791b9924e683
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-17 22:20:45 +0530

    Update get_loan_balance_func.sql

--- Week-2/PL-SQL programming/get_loan_balance_func.sql
+++ Week-2/PL-SQL programming/get_loan_balance_func.sql
@@ -1,1 +1,21 @@
+CREATE OR REPLACE FUNCTION GetLoanBalance(p_LoanID IN NUMBER)
+RETURN NUMBER
+IS
+    v_Balance NUMBER;
+BEGIN
+    SELECT Balance
+    INTO v_Balance
+    FROM Loans
+    WHERE LoanID = p_LoanID;
+
+    RETURN v_Balance;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Loan not found.');
+        RETURN NULL;
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+        RETURN NULL;
+END;
+/
 
