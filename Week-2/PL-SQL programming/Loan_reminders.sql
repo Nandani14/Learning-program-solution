ce328c93488ce2be3e4f79c3b80b1d06e2f3c167
Nandani14 <122161623+Nandani14@users.noreply.github.com>
2025-07-17 22:18:49 +0530

    Update Loan_reminders.sql

--- Week-2/PL-SQL programming/Loan_reminders.sql
+++ Week-2/PL-SQL programming/Loan_reminders.sql
@@ -1,1 +1,15 @@
+BEGIN
+    FOR loan IN (
+        SELECT LoanID, CustomerID, DueDate
+        FROM Loans
+        WHERE DueDate BETWEEN SYSDATE AND SYSDATE + 30
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: Loan ' || loan.LoanID ||
+            ' for Customer ' || loan.CustomerID ||
+            ' is due on ' || TO_CHAR(loan.DueDate, 'DD-MON-YYYY')
+        );
+    END LOOP;
+END;
+/
 
